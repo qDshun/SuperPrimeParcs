2a6cf2ffd5720e63ccb82698d2f5c859016622d0
Ilya Semenov <deff3010@gmail.com>
2021-04-08 10:57:45 +0300

    Test 7 like initial

--- src/Fib.java
+++ src/Fib.java
@@ -1,48 +1,4 @@
-
-// import parcs.*;
-
-// public class Fib implements AM{
-//     public void run(AMInfo info){
-//         long n = info.parent.readLong();
-//         if (n < 3)
-//             return;
-//         info.parent.write(100);
-//             /*
-//         point p1 = info.createPoint();
-//         channel c1 = p1.createChannel();
-//         p1.execute("Fib");
-//         c1.write(n-1);
-//         if (isSuperPrime(n))
-//             info.parent.write(n);*/
-
-//     }
-
-//     public static boolean isSuperPrime(long n)
-//     {
-//         if (!isPrime(n))
-//             return false;
-//         int primesLength = 0;
-//         for(int i=0; i< n+1; i++)
-//         {
-//             if(isPrime(i))
-//                 primesLength++;
-//         }
-//         if(!isPrime(primesLength))
-//             return false;
-//         return true;
-//     }
-
-//     public static boolean isPrime(long n) {
-//         if(n < 2) return false;
-//         if(n == 2 || n == 3) return true;
-//         if(n%2 == 0 || n%3 == 0) return false;
-//         long sqrtN = (long)Math.sqrt(n)+1;
-//         for(long i = 6L; i <= sqrtN; i += 6) {
-//             if(n%(i-1) == 0 || n%(i+1) == 0) return false;
-//         }
-//         return true;
-//     }
-// }import parcs.*;
+import parcs.*;
 
 public class Fib implements AM{
     public void run(AMInfo info){
